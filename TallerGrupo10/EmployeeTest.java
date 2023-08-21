a2b5c07c003b46768d5269d609970bd9616586ab
Angel Tomala <aatomala@espol.edu.ec>
2023-08-21 10:15:18 -0500

    dos casos de prueba salario

--- TallerGrupo10/EmployeeTest.java
+++ TallerGrupo10/EmployeeTest.java
@@ -1,5 +1,6 @@
 
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Test;
 
 import org.junit.jupiter.api.Test;
 
@@ -18,6 +19,21 @@ class EmployeeTest {
 
 	
 
+	
+	@Test
+	public void testCSCurrency() {
+		Employee tmpEmployee = new Employee(500.0, "papas", 4, Worker);
+		float salary = tmpEmployee.cs();
+		assertEquals(0.0, salary);
+		
+	}
+	
+	@Test
+	public void testCSNoEmployee() {
+		Employee tmpEmployee = new Employee(1.1, "$USD", 15, Person);
+		float salary = tmpEmployee.cs();
+		assertEquals(0.0, salary);
+	}
 }
 
 
